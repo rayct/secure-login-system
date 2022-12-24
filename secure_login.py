f10b5bed36bd9fc78071619aeb494f9610f92399
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-12-24 02:29:30 +0000

    Login Server Build Part 1

--- secure_login.py
+++ secure_login.py
@@ -1,1 +1,25 @@
 import sqlite3
+import hashlib
+
+conn = sqlite3.connect("userdata.db")
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS userdata (
+    id INTEGER PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)
+""")
+
+username1, password1 = "ray123", hashlib.sha256("raypassword".encode()).hexdigest()
+username2, password2 = "kalib231", hashlib.sha256("myfarm".encode()).hexdigest()
+username3, password3 = "monty455", hashlib.sha256("montypython".encode()).hexdigest()
+username4, password4 = "jeremy322", hashlib.sha256("clarksonsfarm".encode()).hexdigest()
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username1, password1))
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username2, password2))
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username3, password3))
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username4, password4))
+
+conn.commit()
+
